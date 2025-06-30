01463a99383139393775e7cf123c2b193d44027f
Ishan Rai <135622038+77ishan@users.noreply.github.com>
2025-06-30 08:21:22 +0545

    Update loginPage.js

--- cypress/e2e/pages/loginPage.js
+++ cypress/e2e/pages/loginPage.js
@@ -2,36 +2,27 @@ class LoginPage {
   visit() {
     cy.visit('/login');
   }
-
   fillLoginInfo(username, password) {
     cy.get('input[placeholder="Enter your username here"]').type(username);
     cy.get('input[placeholder="Enter your password here"]').type(password);
   }
-
-
   clickSignInButton() {
     cy.get('button').contains('Sign In Now').click();
   }
-
   assertionEmptyFields() {
     cy.contains('The username field is empty.').should('be.visible');
     cy.contains('The password field is empty.').should('be.visible');
   }
-
-  
   assertLoginButtonVisible() {
     cy.get('button').contains('Sign In Now').should('be.visible');
   }
-
   assertLogoutButtonVisible() {
     cy.get('button.logout').should('be.visible');
     
   }
-
   assertErrorMessageVisible(errorMessage) {
     cy.contains(errorMessage).should('be.visible');
   }
-
   loginWithUnregisteredUser(username, password) {
     this.visit();
     this.fillLoginInfo(username, password);
@@ -41,5 +32,4 @@ class LoginPage {
     this.assertLoginButtonVisible(); 
   }
 }
-
 export default LoginPage;
